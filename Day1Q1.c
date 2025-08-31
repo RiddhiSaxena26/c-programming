f6481d41e4c21bef4f7ff651946fa6ba705b5c21
RiddhiSaxena26 <Riddhi.22853@stu.upes.ac.in>
2025-08-31 16:56:45 +0530

    Add files via upload

---
+++ Day1Q1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+    int a,b;
+    printf("enter number 1 : ");
+    scanf("%d",&a);
+    printf("enter number 2 : ");
+    scanf("%d",&b);
+    int sum = a+b;
+    printf("sum = %d", sum);
+    return 0;
+}
