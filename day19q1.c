1abbc9d154fcccb4f29bb7eb9a19f576ce0d99bc
RiddhiSaxena26 <Riddhi.22853@stu.upes.ac.in>
2025-09-28 19:17:31 +0530

    Add files via upload

---
+++ day19q1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int findHCF(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+int findLCM(int a, int b) {
+    int hcf = findHCF(a, b);
+    return (a * b) / hcf;
+}
+
+int main() {
+    int num1, num2;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    int lcm = findLCM(num1, num2);
+    printf("LCM of %d and %d is %d\n", num1, num2, lcm);
+
+    return 0;
+}
