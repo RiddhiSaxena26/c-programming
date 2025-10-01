8cce46050b1ca64f65c20a93d28974a36867f78d
RiddhiSaxena26 <Riddhi.22853@stu.upes.ac.in>
2025-10-01 10:31:33 +0530

    Add files via upload

---
+++ day33q1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int target) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target)
+            return mid; // Found at index mid
+        else if (arr[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return -1; // Not found
+}
+
+int main() {
+    int arr[] = {2, 4, 6, 10, 8, 12, 14};
+    int n=7;
+    int target = 10;
+
+    int result = binarySearch(arr, n, target);
+
+    if (result != -1)
+        printf("Element %d found at index %d\n", target, result);
+    else
+        printf("Element %d not found\n", target);
+
+    return 0;
+}
---
+++ day34q1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100] = {10, 20, 30, 40, 50}; 
+    int n = 5;
+    int pos = 2; 
+    int element = 25; 
+    for (int i = n; i > pos; i--)
+        arr[i] = arr[i - 1];
+
+   
+    arr[pos] = element;
+    n++;
+
+    printf("Updated array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
